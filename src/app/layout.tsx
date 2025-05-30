eb54afc68832202ead1a3e2bfb2b606a9e836c5c
saiful <muhammad.saiful.engineer@gmail.com>
2025-05-30 14:51:24 +0700

    fix(favicon): fix missing favicon google search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+       <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body className={inter.variable}>
         {children}
       </body>
